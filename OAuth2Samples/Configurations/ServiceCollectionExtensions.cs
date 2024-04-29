240f199e7e103da31a867dfab85aa0c673677ccc
Umarkulov Mukhammad <96643134+MuhammadUmarqulov@users.noreply.github.com>
2024-04-29 11:10:51 +0500

    initial commit

---
+++ OAuth2Samples/Configurations/ServiceCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
+
+namespace OAuth2Samples;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddAuthenticationWithOAuth2Config(
+		this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddAuthentication("Cookie")
+			.AddCookie("Cookie")
+			.AddTwitter("X", t =>
+			{
+				t.SignInScheme = "Cookie";
+
+				t.ConsumerKey = configuration["OAuth2.0:X:ClientId"];
+				t.ConsumerSecret = configuration["OAuth2.0:X:ClientSecret"];
+				t.CallbackPath = "/any";
+				t.SaveTokens = true;
+            })
+			.AddOAuth("GitHub", g =>
+			{
+				g.SignInScheme = "Cookie";
+
+				g.ClientId = configuration["OAuth2.0:GitHub:ClientId"];
+				g.ClientSecret = configuration["OAuth2.0:GitHub:ClientSecret"];
+
+				g.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+				g.TokenEndpoint = "https://github.com/login/oauth/access_token";
+				g.CallbackPath = "/any";
+				g.SaveTokens = true;
+				g.UserInformationEndpoint = "https://api.github.com/user";
+
+				g.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+				g.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+
+                g.Events.OnCreatingTicket = async ctx =>
+				{
+					using var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+					request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+					using var result = await ctx.Backchannel.SendAsync(request);
+					var user = await result.Content.ReadFromJsonAsync<JsonElement>();
+					ctx.RunClaimActions(user);
+				};
+            });
+		return services;
+	}
+}
+
---
+++ OAuth2Samples/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using OAuth2Samples;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthenticationWithOAuth2Config(builder.Configuration);
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapGet("/", (HttpContext ctx) =>
+{
+    return ctx.User.Claims.Select(x => new { x.Type, x.Value }).ToList();
+}); 
+
+app.MapGet("/git-hub", () => {
+    return Results.Challenge(
+        new AuthenticationProperties()
+        {
+            RedirectUri = "https://localhost:7114/"
+        },
+        new List<string> { "GitHub" });
+});
+
+app.MapGet("/twitter", () => {
+    return Results.Challenge(
+        new AuthenticationProperties()
+        {
+            RedirectUri = "https://localhost:7114/"
+        },
+        new List<string> { "X" });
+});
+app.Run();
+
